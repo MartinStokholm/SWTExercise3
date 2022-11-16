ab3c51306d7e98690ba9b2e1a9ba282bdb5235d8
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2022-11-16 16:15:09 +0100

    BuzzerTest complete

---
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit
+{
+    public class BuzzerTest
+    {
+        private Buzzer uut;
+        private IOutput output;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            output = Substitute.For<IOutput>();
+            uut = new Buzzer(output);
+        }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(1)]
+        [TestCase(0)]
+
+        public void BuzzerCalledWithAmount(int param)
+        {
+            uut.Buzz(param, 3);
+
+            output.Received(param).OutputLine(Arg.Is<string>(str => str.Contains("Buzzing for " + 3 + " ms")));
+
+        }
+
+        [Test]
+        [TestCase(-1)]
+        public void BuzzerCalledWithIllegalAmount(int param)
+        {
+            Assert.That(() => uut.Buzz(param, 3), Throws.TypeOf<ArgumentException>());
+
+        }
+
+    }
+}
+
