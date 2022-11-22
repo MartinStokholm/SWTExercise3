32895fae637ae3c83fdb73cf6665a063704fff5e
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-22 12:04:23 +0100

    removed wrong test

--- Microwave.Test.Unit/UserInterfaceTest.cs
+++ Microwave.Test.Unit/UserInterfaceTest.cs
@@ -244,21 +244,6 @@ namespace Microwave.Test.Unit
         }
 
         [Test]
-        public void SetTime_TimeButton_WhileCooking()
-        {
-            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
-            // Now in SetPower
-            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
-            // Now in SetTime
-            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
-            // Now cooking
-            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
-
-        }
-
-
-
-        [Test]
         public void Cooking_CookingIsDone_LightOff()
         {
             powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
